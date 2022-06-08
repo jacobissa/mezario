9c973ebf515188da1487f2d3a5463a8b846519d6
DerbasMo73636 <derbasmo73636@th-nuernberg.de>
2022-06-08 16:24:43 +0200

    addition

--- Mezario/E_Alpha.cpp
+++ Mezario/E_Alpha.cpp
@@ -10,21 +10,54 @@ PositionPtr E_Alpha::GetNextPosition(const PositionPtr& ptr_position_player)
 {
 	if (mptr_position_current->IsClose(ptr_position_player->GetPosition(), 5))
 	{
-		if (mptr_position_current->GetPosition().x > ptr_position_player->GetPosition().x)
+		if (mptr_position_current->GetPosition().x > ptr_position_player->GetPosition().x )
 		{
-			return std::make_shared<Position>(mptr_position_current->GetLeftPosition());
+			if (mptr_position_current->GetLeftPosition().x != Cell::e_cell_wall && 
+				!mptr_position_current->IsClose(ptr_position_player->GetPosition(), 2))
+			{
+				return std::make_shared<Position>(mptr_position_current->GetLeftPosition());
+			}
+			else if(mptr_position_current->IsClose(ptr_position_player->GetPosition(), 2))
+			{
+				return std::make_shared<Position>(mptr_position_current->GetPosition());
+			}
+			
 		}
 		else if (mptr_position_current->GetPosition().x < ptr_position_player->GetPosition().x)
 		{
-			return  std::make_shared<Position>(mptr_position_current->GetRightPosition());
+			if (mptr_position_current->GetRightPosition().x != Cell::e_cell_wall && 
+				!mptr_position_current->IsClose(ptr_position_player->GetPosition(), 2))
+			{
+				return  std::make_shared<Position>(mptr_position_current->GetRightPosition());
+			}
+			else if(mptr_position_current->IsClose(ptr_position_player->GetPosition(), 2))
+			{
+				return  std::make_shared<Position>(mptr_position_current->GetPosition());
+			}
 		}
 		else if (mptr_position_current->GetPosition().y < ptr_position_player->GetPosition().y)
 		{
-			return  std::make_shared<Position>(mptr_position_current->GetDownPosition());
+			if (mptr_position_current->GetDownPosition().y != Cell::e_cell_wall && 
+				!mptr_position_current->IsClose(ptr_position_player->GetPosition(), 2))
+			{
+				return  std::make_shared<Position>(mptr_position_current->GetDownPosition());
+			}
+			else if(mptr_position_current->IsClose(ptr_position_player->GetPosition(), 2))
+			{
+				return  std::make_shared<Position>(mptr_position_current->GetPosition());
+			}
 		}
 		else if (mptr_position_current->GetPosition().y > ptr_position_player->GetPosition().y)
 		{
-			return  std::make_shared<Position>(mptr_position_current->GetUpPosition());
+			if (mptr_position_current->GetUpPosition().y != Cell::e_cell_wall && 
+				!mptr_position_current->IsClose(ptr_position_player->GetPosition(), 2))
+			{
+				return  std::make_shared<Position>(mptr_position_current->GetUpPosition());
+			}
+			else if(mptr_position_current->IsClose(ptr_position_player->GetPosition(), 2))
+			{
+				return  std::make_shared<Position>(mptr_position_current->GetPosition());
+			}
 		}
 
 	}
