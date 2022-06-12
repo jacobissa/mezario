d1ac3827dc3552adc26dd10629835dcbb651ca86
issaja73014 <issaja73014@th-nuernberg.de>
2022-06-12 19:46:49 +0200

    remove GetNextPosition() from enemy.cpp

--- Mezario/Enemy.cpp
+++ Mezario/Enemy.cpp
@@ -1,12 +1,6 @@
 #include "Enemy.h"
 
-Enemy::Enemy(PositionPtr& ptr_position, enum Cell e_cell_creature, const CellShot s_cell_shot)
-	: Creature(ptr_position, e_cell_creature, s_cell_shot)
+Enemy::Enemy(PositionPtr& ptr_position , enum Cell e_cell_creature , const CellShot s_cell_shot)
+	: Creature(ptr_position , e_cell_creature , s_cell_shot)
 {
 }
-
-PositionPtr Enemy::GetNextPosition(const PositionPtr& ptr_position_player)
-{
-	return 	std::make_shared<Position>(mptr_position_current->GetPosition());
-}
-
