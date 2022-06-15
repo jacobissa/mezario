e5c7cce3b1ce164b10da6d6aa67dfeb15be17180
issaja73014 <issaja73014@th-nuernberg.de>
2022-06-16 00:30:59 +0200

    Update Playground.cpp

--- Mezario/Playground.cpp
+++ Mezario/Playground.cpp
@@ -5,16 +5,16 @@
 
 #include "Alpha.h"
 
-Playground::Playground(const int i_height, const int i_width, const int i_hearts, const int i_probability_wall, const int i_quantity_enemy, const int i_time_max)
+Playground::Playground(const int i_height , const int i_width , const int i_hearts , const int i_probability_wall , const int i_quantity_enemy , const int i_time_max)
 	: mi_height(i_height)
 	, mi_width(i_width)
 	, mi_hearts(i_hearts)
 	, mi_probability_wall(i_probability_wall)
 	, mi_quantity_enemy(i_quantity_enemy)
 	, mi_time_max(i_time_max)
-	, mptr_position_exit(std::make_shared<Position>(i_width - 1, i_height - 4))
+	, mptr_position_exit(std::make_shared<Position>(i_width - 1 , i_height - 4))
 {
-	PositionPtr ptr_position_player = std::make_shared<Position>(0, 3);
+	PositionPtr ptr_position_player = std::make_shared<Position>(0 , 3);
 	mptr_player = std::make_shared<Player>(ptr_position_player);
 	mptr_matrix = new char* [mi_height];
 	for ( int y = 0; y < mi_height; y++ )
@@ -58,8 +58,8 @@ void Playground::PlayerShot() const
 {
 	if ( mptr_player->IsShotActive() )
 	{
-		SetValue(mptr_player->GetShotPreviousPosition(), Cell::e_cell_blank);
-		SetValue(mptr_player->GetShotCurrentPosition(), Cell::e_cell_blank);
+		SetValue(mptr_player->GetShotPreviousPosition() , Cell::e_cell_blank);
+		SetValue(mptr_player->GetShotCurrentPosition() , Cell::e_cell_blank);
 	}
 	mptr_player->StartShot();
 }
@@ -78,13 +78,13 @@ void Playground::UpdateCreatures()
 
 void Playground::PrintToConsole(const HANDLE& h_console) const
 {
-	const PositionPtr ptr_position_cell = std::make_shared<Position>(0, 0);
+	const PositionPtr ptr_position_cell = std::make_shared<Position>(0 , 0);
 	for ( int y = 0; y < mi_height; y++ )
 	{
 		for ( int x = 0; x < mi_width; x++ )
 		{
-			ptr_position_cell->UpdatePosition(x, y);
-			PrintCell(h_console, static_cast<enum Cell>(GetValue(ptr_position_cell)));
+			ptr_position_cell->UpdatePosition(x , y);
+			PrintCell(h_console , static_cast<enum Cell>( GetValue(ptr_position_cell) ));
 		}
 		std::cout << std::endl;
 	}
@@ -102,7 +102,7 @@ int Playground::GetHearts() const
 
 int Playground::GetEnemies() const
 {
-	return static_cast<int>(mvec_enemy.size());
+	return static_cast<int>( mvec_enemy.size() );
 }
 
 bool Playground::IsWin() const
@@ -118,10 +118,10 @@ bool Playground::IsLose() const
 void Playground::Initialize()
 {
 	const PositionPtr ptr_position_player = mptr_player->GetCurrentPosition();
-	const int i_enemy_position = ((mi_height - 2) * (mi_width - 2)) / (mi_quantity_enemy);
+	const int i_enemy_position = ( ( mi_height - 2 ) * ( mi_width - 2 ) ) / ( mi_quantity_enemy );
 	const int i_probability_enemy_position = rand() % i_enemy_position;
 	int i_counter = 1;
-	const PositionPtr ptr_position_cell = std::make_shared<Position>(0, 0);
+	const PositionPtr ptr_position_cell = std::make_shared<Position>(0 , 0);
 
 	for ( int y = 0; y < mi_height; y++ )
 	{
@@ -129,62 +129,62 @@ void Playground::Initialize()
 
 		for ( int x = 0; x < mi_width; x++ )
 		{
-			ptr_position_cell->UpdatePosition(x, y);
+			ptr_position_cell->UpdatePosition(x , y);
 
 			const int i_probability_wall = rand() % 100;
 			if ( ptr_position_cell->Equals(ptr_position_player->GetPosition())
 				|| ptr_position_cell->Equals(mptr_position_exit->GetPosition()) )
 			{
 				// keep the start end exit positions empty
-				SetValue(ptr_position_cell, Cell::e_cell_blank);
+				SetValue(ptr_position_cell , Cell::e_cell_blank);
 			}
 			else if ( x == 0 || x == mi_width - 1 || y == 0 || y == mi_height - 1 )
 			{
 				// draw the walls on bounds
-				SetValue(ptr_position_cell, Cell::e_cell_wall);
+				SetValue(ptr_position_cell , Cell::e_cell_wall);
 			}
 			else if ( i_counter % i_enemy_position == i_probability_enemy_position && mi_quantity_enemy > 0 )
 			{
 				// create the enemies
-				PositionPtr ptr_position_enemy = std::make_shared<Position>(x, y);
+				PositionPtr ptr_position_enemy = std::make_shared<Position>(x , y);
 				AlphaPtr ptr_enemy = std::make_shared<Alpha>(ptr_position_enemy);
 				mvec_enemy.emplace_back(ptr_enemy);
-				SetValue(ptr_position_cell, Cell::e_cell_blank);
+				SetValue(ptr_position_cell , Cell::e_cell_blank);
 				mi_quantity_enemy--;
 				i_counter++;
 			}
 			else if ( x == i_heart_position_x )
 			{
-				SetValue(ptr_position_cell, Cell::e_cell_heart);
+				SetValue(ptr_position_cell , Cell::e_cell_heart);
 				i_counter++;
 			}
 			else if ( i_probability_wall < mi_probability_wall
-					 && !ptr_position_player->IsClose(ptr_position_cell->GetPosition(), 3)
-					 && !mptr_position_exit->IsClose(ptr_position_cell->GetPosition(), 3) )
+					 && !ptr_position_player->IsClose(ptr_position_cell->GetPosition() , 3)
+					 && !mptr_position_exit->IsClose(ptr_position_cell->GetPosition() , 3) )
 			{
 				// draw the walls inside the maze, except the area near start & exit positions
-				SetValue(ptr_position_cell, Cell::e_cell_wall);
+				SetValue(ptr_position_cell , Cell::e_cell_wall);
 				i_counter++;
 			}
-			else if ( i_probability_wall < (mi_probability_wall * 1.5)
-					 && !ptr_position_player->IsClose(ptr_position_cell->GetPosition(), 3)
-					 && !mptr_position_exit->IsClose(ptr_position_cell->GetPosition(), 3) )
+			else if ( i_probability_wall < ( mi_probability_wall * 1.5 )
+					 && !ptr_position_player->IsClose(ptr_position_cell->GetPosition() , 3)
+					 && !mptr_position_exit->IsClose(ptr_position_cell->GetPosition() , 3) )
 			{
 				// draw the obstacles inside the maze, except the area near start & exit positions
-				SetValue(ptr_position_cell, Cell::e_cell_obstacle);
+				SetValue(ptr_position_cell , Cell::e_cell_obstacle);
 				i_counter++;
 			}
 			else
 			{
 				// fill the rest with empty char
-				SetValue(ptr_position_cell, Cell::e_cell_blank);
+				SetValue(ptr_position_cell , Cell::e_cell_blank);
 				i_counter++;
 			}
 		}
 	}
 }
 
-void Playground::SetValue(const PositionPtr& ptr_position, const Cell e_cell) const
+void Playground::SetValue(const PositionPtr& ptr_position , const Cell e_cell) const
 {
 	if ( IsInBounds(ptr_position) )
 	{
@@ -224,7 +224,7 @@ void Playground::UpdateEnemyShot(const EnemyPtr& ptr_enemy)
 
 	if ( !ptr_enemy->IsShotActive() )
 	{
-		ptr_enemy->StartShot(mptr_player->GetCurrentPosition(), GetTimeCounter());
+		ptr_enemy->StartShot(mptr_player->GetCurrentPosition() , GetTimeCounter());
 	}
 	else
 	{
@@ -270,20 +270,20 @@ void Playground::UpdateEnemyShot(const EnemyPtr& ptr_enemy)
 					case Cell::e_cell_obstacle:
 					case Cell::e_cell_heart:
 						{
-							SetValue(ptr_position_shot_current, Cell::e_cell_blank);
+							SetValue(ptr_position_shot_current , Cell::e_cell_blank);
 							ptr_enemy->StopShot();
 						}
 						break;
 					default:;
 				}
 			}
-			SetValue(ptr_position_shot_previous, Cell::e_cell_blank);
+			SetValue(ptr_position_shot_previous , Cell::e_cell_blank);
 		}
 		if ( ptr_position_shot_current && IsInBounds(ptr_position_shot_current) && GetValue(ptr_position_shot_current) == Cell::e_cell_wall )
 		{
 			// remove shot, if faced a wall
 			ptr_enemy->StopShot();
-			SetValue(ptr_position_shot_previous, Cell::e_cell_blank);
+			SetValue(ptr_position_shot_previous , Cell::e_cell_blank);
 		}
 	}
 }
@@ -320,15 +320,15 @@ void Playground::UpdatePlayerShot()
 						{
 							// Player's Shot kills an enemy
 							mvec_enemy.erase(
-								std::remove_if(mvec_enemy.begin(), mvec_enemy.end(),
-											   [&](const EnemyPtr& ptr_enemy)
+								std::remove_if(mvec_enemy.begin() , mvec_enemy.end() ,
+											   [&] (const EnemyPtr& ptr_enemy)
 							{
 								if ( ptr_enemy->GetCurrentPosition()->Equals(ptr_position_shot_current->GetPosition()) )
 								{
-									SetValue(ptr_enemy->GetCurrentPosition(), Cell::e_cell_blank);
+									SetValue(ptr_enemy->GetCurrentPosition() , Cell::e_cell_blank);
 									if ( ptr_enemy->IsShotActive() )
 									{
-										SetValue(ptr_enemy->GetShotCurrentPosition(), Cell::e_cell_blank);
+										SetValue(ptr_enemy->GetShotCurrentPosition() , Cell::e_cell_blank);
 										ptr_enemy->StopShot();
 									}
 									return true;
@@ -337,7 +337,7 @@ void Playground::UpdatePlayerShot()
 								{
 									return false;
 								}
-							}),
+							}) ,
 								mvec_enemy.end());
 							mptr_player->StopShot();
 						}
@@ -349,7 +349,7 @@ void Playground::UpdatePlayerShot()
 							{
 								if ( ptr_enemy->IsShotActive() && ptr_enemy->GetShotCurrentPosition()->Equals(ptr_position_shot_current->GetPosition()) )
 								{
-									SetValue(ptr_enemy->GetShotCurrentPosition(), Cell::e_cell_blank);
+									SetValue(ptr_enemy->GetShotCurrentPosition() , Cell::e_cell_blank);
 									ptr_enemy->StopShot();
 								}
 							}
@@ -359,14 +359,14 @@ void Playground::UpdatePlayerShot()
 					case Cell::e_cell_obstacle:
 					case Cell::e_cell_heart:
 						{
-							SetValue(ptr_position_shot_current, Cell::e_cell_blank);
+							SetValue(ptr_position_shot_current , Cell::e_cell_blank);
 							mptr_player->StopShot();
 						}
 						break;
 					default:;
 				}
 			}
-			SetValue(ptr_position_shot_previous, Cell::e_cell_blank);
+			SetValue(ptr_position_shot_previous , Cell::e_cell_blank);
 		}
 	}
 }
@@ -378,70 +378,70 @@ void Playground::UpdateCreature(const CreaturePtr& ptr_creature) const
 	const PositionPtr ptr_position_shot_current = ptr_creature->GetShotCurrentPosition();
 	const PositionPtr ptr_position_shot_previous = ptr_creature->GetShotPreviousPosition();
 
-	const PositionPtr ptr_position_cell = std::make_shared<Position>(0, 0);
+	const PositionPtr ptr_position_cell = std::make_shared<Position>(0 , 0);
 	for ( int y = 0; y < mi_height; y++ )
 	{
 		for ( int x = 0; x < mi_width; x++ )
 		{
-			ptr_position_cell->UpdatePosition(x, y);
+			ptr_position_cell->UpdatePosition(x , y);
 
 			if ( ptr_position_cell->Equals(ptr_position_current->GetPosition()) )
 			{
 				// new position of creature
-				SetValue(ptr_position_cell, ptr_creature->GetCellCreature());
+				SetValue(ptr_position_cell , ptr_creature->GetCellCreature());
 			}
 			else if ( ptr_position_cell->Equals(ptr_position_previous->GetPosition()) && GetValue(ptr_position_cell) == ptr_creature->GetCellCreature() )
 			{
 				// remove old position of creature
-				SetValue(ptr_position_cell, Cell::e_cell_blank);
+				SetValue(ptr_position_cell , Cell::e_cell_blank);
 			}
 			else if ( ptr_creature->IsShotActive() && ptr_position_cell->Equals(ptr_position_shot_current->GetPosition()) )
 			{
 				// new position of creature's shot
-				SetValue(ptr_position_cell, ptr_creature->GetCellShot());
+				SetValue(ptr_position_cell , ptr_creature->GetCellShot());
 			}
 			else if ( ptr_creature->IsShotActive() && ptr_position_cell->Equals(ptr_position_shot_previous->GetPosition())
 					 && GetValue(ptr_position_cell) == ptr_creature->GetCellShot() )
 			{
 				// remove old position of creature's shot
-				SetValue(ptr_position_cell, Cell::e_cell_blank);
+				SetValue(ptr_position_cell , Cell::e_cell_blank);
 			}
 		}
 	}
 }
 
 
-void Playground::PrintCell(const HANDLE& h_console, const Cell e_cell)
+void Playground::PrintCell(const HANDLE& h_console , const Cell e_cell)
 {
 	switch ( e_cell )
 	{
 		case Cell::e_cell_blank:
 			{
-				SetConsoleTextAttribute(h_console, Color::e_color_black);
+				SetConsoleTextAttribute(h_console , Color::e_color_black);
 				std::cout << e_cell;
 			}
 			break;
 		case Cell::e_cell_wall:
 			{
-				SetConsoleTextAttribute(h_console, Color::e_color_brown);
+				SetConsoleTextAttribute(h_console , Color::e_color_brown);
 				std::cout << e_cell;
 			}
 			break;
 		case Cell::e_cell_obstacle:
 			{
-				SetConsoleTextAttribute(h_console, Color::e_color_white);
+				SetConsoleTextAttribute(h_console , Color::e_color_white);
 				std::cout << e_cell;
 			}
 			break;
 		case Cell::e_cell_heart:
 			{
-				SetConsoleTextAttribute(h_console, Color::e_color_cyan);
+				SetConsoleTextAttribute(h_console , Color::e_color_cyan);
 				std::cout << e_cell;
 			}
 			break;
 		case Cell::e_cell_player:
 			{
-				SetConsoleTextAttribute(h_console, Color::e_color_green);
+				SetConsoleTextAttribute(h_console , Color::e_color_green);
 				std::cout << e_cell;
 			}
 			break;
@@ -449,7 +449,7 @@ void Playground::PrintCell(const HANDLE& h_console, const Cell e_cell)
 		case Cell::e_cell_enemy_beta:
 		case Cell::e_cell_enemy_gamma:
 			{
-				SetConsoleTextAttribute(h_console, Color::e_color_red);
+				SetConsoleTextAttribute(h_console , Color::e_color_red);
 				std::cout << e_cell;
 			}
 			break;
@@ -458,13 +458,13 @@ void Playground::PrintCell(const HANDLE& h_console, const Cell e_cell)
 		case Cell::e_cell_player_shot_left:
 		case Cell::e_cell_player_shot_right:
 			{
-				SetConsoleTextAttribute(h_console, Color::e_color_light_green);
+				SetConsoleTextAttribute(h_console , Color::e_color_light_green);
 				std::cout << e_cell;
 			}
 			break;
 		case Cell::e_cell_enemy_shot_alpha:
 			{
-				SetConsoleTextAttribute(h_console, Color::e_color_light_red);
+				SetConsoleTextAttribute(h_console , Color::e_color_light_red);
 				std::cout << e_cell;
 			}
 			break;
@@ -481,6 +481,6 @@ int Playground::GetTimeCounter() const
 		return 0;
 	}
 	const auto time_current = std::chrono::steady_clock::now();
-	const auto duration_time = std::chrono::duration_cast<std::chrono::duration<int>>(time_current - mtime_start).count();
+	const auto duration_time = std::chrono::duration_cast<std::chrono::duration<int>>( time_current - mtime_start ).count();
 	return duration_time;
 }
